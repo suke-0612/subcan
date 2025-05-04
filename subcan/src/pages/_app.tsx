03dd425e96a3e34ee10700e44fd2e850b6f36d60
sota <sota1210nittc@gmail.com>
2025-05-04 18:01:41 +0900

    ヘッダーの仮作成

--- subcan/src/pages/_app.tsx
+++ subcan/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
+import Header from "./Header";
 
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <AuthGuard>
+        <Header />
         <Component {...pageProps} />
       </AuthGuard>
     </AuthProvider>
