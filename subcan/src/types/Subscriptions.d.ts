2c921fef5691ab3c44c5e46e879b6a3ef1ce082a
sota <sota1210nittc@gmail.com>
2025-05-05 11:20:07 +0900

    Subscriptionの型ファイル追加

---
+++ subcan/src/types/Subscriptions.d.ts
@@ -0,0 +1,15 @@
+interface Subscription {
+  user_id: string; // ユーザーID
+  name: string; // サブスク名
+  fee: number; // 料金
+  period: string; // サブスク期間
+  payment_starts_at: string; // 支払い開始日
+  payment_period: string; // 支払い期間
+  last_payment_date: string; // 最後の支払日
+  frequency: number; // ユーザーの利用頻度
+  icon: string; // アイコンへのパス
+  trial_period: string; // 無料期間
+  cancel_url?: string; // 解約先URL
+  created_at: string;
+  updated_at: string;
+}
