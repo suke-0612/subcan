a2fe1bc08767c5476e512a63699490fb51a0b4c7
rei <homahoma2909@gmail.com>
2025-05-05 17:40:16 +0900

    register googlebutton

--- subcan/src/app/(protected)/register/page.tsx
+++ subcan/src/app/(protected)/register/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import GoogleCalendarButton from "@/components/GoogleComponentButton";
 
 type Props = object; // Propsの型をここに定義
 
@@ -10,6 +12,11 @@ const MyComponent: React.FC<Props> = () => {
     { label: "更新頻度", value: "" },
     { label: "利用頻度", value: "" },
   ];
+  const eventTitle = "打ち合わせ";
+  const description = "クライアントとの定例ミーティング";
+  const location = "Zoom";
+  const start = new Date("2025-05-10T15:00:00+09:00");
+  const end = new Date("2025-05-10T16:00:00+09:00");
 
   return (
     <div style={{ paddingTop: "30px" }}>
@@ -77,13 +84,34 @@ const MyComponent: React.FC<Props> = () => {
           ))}
         </div>
       </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginTop: 10,
+          marginBottom: 30,
+        }}
+      >
+        <GoogleCalendarButton
+          title={eventTitle}
+          description={description}
+          location={location}
+          startTime={start}
+          endTime={end}
+        />
+      </div>
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
         <button
           style={{
             backgroundColor: "white",
-            border: "2px solid #3C6E71",
+            border: "3px solid #3C6E71",
             borderRadius: "7px ",
-            width: "20%",
+            width: 150,
+            height: 40,
+            fontSize: 22,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           登録
